9e24ab35a84c58c6edcf11fe18a2833a5503b3f8
Rom Grk <romgrk.cc@gmail.com>
2018-12-19 17:07:18 -0500

    update help

--- src/components/Help.js
+++ src/components/Help.js
@@ -20,18 +20,18 @@ function Help(props) {
 
         <h2>What is this tool?</h2>
         <p className='Help__text'>
-          The Circular DNA Re-aligner allows you to provide your bacterial DNA sequence
-          and a desired start sequence, and realign the DNA sequence to match this start.
-          It also reverse your DNA sequence if it appears to be in the opposite direction
+          The Bacterial Genome Synchronizer allows you to provide your bacterial genome
+          and a desired start sequence, and realign the genome to start at this sequence.
+          It also reverses your genome if it appears to be in the opposite direction
           to your start sequence.
         </p>
 
         <h2>How does it work?</h2>
         <p className='Help__text'>
-          First, you must provide your input DNA sequences, either as FASTA files, or as NCBI
+          First, you must provide your input genomes, either as FASTA files, or as NCBI
           accession numbers. <br/>
-          Second, you need to provide your start sequence, either from a text file, or in the
-          text input. <br/>
+          Second, you need to provide your start sequence, either from a text or fasta file,
+          or through the text input field. <br/>
           Third, click <code>Re-Align</code> and let the tool run. <br/>
           Lastly, you can download the files that were realigned.
         </p>
