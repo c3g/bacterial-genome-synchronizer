0b67624768256c1a1acf5a98ed3857492c515dbe
Rom Grk <romgrk.cc@gmail.com>
2019-02-22 12:24:44 -0500

    fix scrolling issue & add scrollbar styles

--- src/index.js
+++ src/index.js
@@ -4,6 +4,7 @@ import ReactDOM from 'react-dom';
 import 'font-awesome/css/font-awesome.min.css';
 import 'flexboxgrid/dist/flexboxgrid.min.css';
 
+import './styles/scrollbar.scss';
 import './styles/spinner.scss';
 import './styles/Button.scss'
 import './styles/Dropdown.scss'
